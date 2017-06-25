19f2faa5f1033652c46ab8317dce7e30eda7789c
Jedidiah Esposito <egerthe@gmail.com>
2017-06-24 17:58:05 -0700

    creating 25 random sets of 3 image objects

--- js/app.js
+++ js/app.js
@@ -1,22 +1,60 @@
 'use strict';
 
-var img1 = document.getElementById('img1')[0];
-var img2 = document.getElementById('img2')[0];
-var img3 = document.getElementById('img3')[0];
+//used jsbin to figure the random piece out https://jsbin.com/pubewewupo/edit?js,console
+
+var allSets = [];
+var images = [];
 
 function Image(name,path) {
+  images.push(this);
   this.name = name;
   this.path = path;
   this.shown = 0;
   this.clicked = 0;
 }
 
-var images = [
-  new Image('bag','img/bag.jpg'),
-  new Image('banana','img/banana.jpg'),
-  new Image('bathroom','img/bathroom.jpg')
-];
+var bag = new Image('Bag','img/bag.jpg');
+var banana = new Image('Banana','img/banana.jpg');
+var bathroom = new Image('Bathroom','img/bathroom.jpg');
+var boots = new Image('Boots','img/boots.jpg');
+var breakfast = new Image('Breakfast','img/breakfast.jpg');
+var bubblegum = new Image('Bubblegum','img/bubblegum.jpg');
+var chair = new Image('Chair','img/chair.jpg');
+var cthulhu = new Image('Cthulhu','img/cthulhu.jpg');
+var dogDuck = new Image('Dog Duck','img/dog-duck.jpg');
+var dragon = new Image('Dragon','img/dragon.jpg');
+var pen = new Image('Pen','img/pen.jpg');
+var petSweep = new Image('Pet Sweep','img/pet-sweep.jpg');
+var scissors = new Image('Scissors','img/scissors.jpg');
+var shark = new Image('Shark','img/shark.jpg');
+var sweep = new Image('Sweep','img/sweep.png');
+var tauntaun = new Image('Taun Taun','img/tauntaun.jpg');
+var unicorn = new Image('Unicorn','img/unicorn.jpg');
+var usb = new Image('USB','img/usb.gif');
+var waterCan = new Image('Water Can','img/water-can.jpg');
+var wineGlass = new Image('Wine Glass','img/wine-glass.jpg');
 
-// var bag = new Image('bag','img/bag.jpg');
-// var banana = new Image('banana','img/banana.jpg');
-// var bathroom = new Image('bathroom','img/bathroom.jpg');
+function randomize() {
+  images = images.filter(function(elem, index, self) {return index == self.indexOf(elem);}).sort(function() {return 0.5 - Math.random();});
+  return images;
+}
+
+function makeSets() {
+  var prevSet = [];
+  var tempSet = [];
+  while (allSets.length < 25) {
+    tempSet = [];
+    var randObj = [];
+    while (tempSet.length < 3) {
+      var randomSet = randomize();
+      randObj = randomSet.slice(0, 1)[0];
+      if (tempSet.indexOf(randObj) === -1 && prevSet.indexOf(randObj) === -1) {
+        tempSet.push(randObj);
+      }
+    }
+    allSets.push(tempSet);
+    prevSet = tempSet;
+  }
+}
+makeSets();
+console.log(allSets);
---
+++ user_stories.md
@@ -0,0 +1,23 @@
+
+#User Stories
+
+## Marketing Research Team
+* track how many times a user clicked (should be 25 each time)
+* track how many times each item is clicked
+* track the percentage of times that an item was clicked when it was among those shown
+## Focus Group Participant
+* instructions for the exercise
+* an intuitive interface
+* clearly see each image 
+## Developer
+* make it real purty with a nice font, palette and semantic layout
+* select three random images from the image directory and display them side-by-side in the browser window
+* receive and track clicks on corresponding displayed objects
+* images should not duplicate in the set of 3 or with the previous set of 3
+* each image object should contain:
+    * image name (for display)
+    * image file-path
+    * number of times it has been displayed
+    * number of times it has been clicked
+    * an id to be used on the DOM
+* After 25 clicks turn the event listeners off and display a list of all of the products with votes received: "3 votes for the Banana."
